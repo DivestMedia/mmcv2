f295bf75b6dfa1a3d6a0ab1ff62333d0dfa3fa04
ljopleda <ljopleda@gmail.com>
2016-09-02 07:08:21 +0800

    Style for glossary page

--- assets/mmc.js
+++ assets/mmc.js
@@ -9,4 +9,13 @@ jQuery(function($){
             $(this).addClass('img-responsive');
         });
     }
+    $(window).load(function() {
+	    $("body").find("#container-glossary").find(".cminds_poweredby").remove();
+	    $("#glossaryList").find(".glossaryLink").each(function() {
+	        var e = $(this).data("cmtooltip");
+	        $(this).removeData("cmtooltip").removeAttr("data-cmtooltip").clone().appendTo($(this).parent()); 
+	        $(this).parent().append("<div>" + e + "</div>"), $(this).remove();
+	    })
+	    $("#glossaryList-nav .ln-letters").find("a:first-child").trigger("click"); 
+    });
 });
