9f165b8e49e3876005cab7859a781a5a86e35532
ljopleda <ljopleda@gmail.com>
2016-09-01 15:57:42 +0800

    Adding general page

---
+++ partials/page.php
@@ -0,0 +1,16 @@
+<section>
+	<div class="container cont-<?=basename(get_permalink())?>">
+		<div class="col-md-12">
+			<header class="margin-bottom-30">
+				<h2 class="section-title"><?=the_title()?></h2>
+			</header>
+			<?php while ( have_posts() ) : the_post();?>
+				<article id="post-<?php the_ID(); ?>">
+					<div class="text-black size-14 entry-content post-<?=get_post_format();?>">
+						<? the_content();?>
+					</div>
+				</article>
+			<?php endwhile;?>
+		</div>
+	</div>
+</section>
