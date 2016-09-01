b2602d7bb7c8eda5f902dec47136a21c32ebeaea
ljopleda <ljopleda@gmail.com>
2016-09-01 10:17:14 +0800

    Adding single post page

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php
 global $post;
 get_header();
-get_template_part("partials/content", locate_template('partials/content-single-'.get_post_type().'.php')!='' ? 'single-'.get_post_type() : 'single' );
+get_template_part("partials/single", locate_template('partials/single-'.get_post_type().'.php')!='' ? get_post_type() : '' );
 get_footer();
