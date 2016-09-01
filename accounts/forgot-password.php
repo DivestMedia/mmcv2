f6cc024164abca85eecdbaf7170122b5caec146f
ljopleda <ljopleda@gmail.com>
2016-09-01 14:30:10 +0800

    Adding account pages

---
+++ accounts/forgot-password.php
@@ -0,0 +1,13 @@
+<?php
+  get_header();
+?>
+<section>
+	<header class="text-center margin-bottom-30">
+		<h2 class="section-title">Forgot Password</h2>
+	</header>
+	<div class="container">
+	    <?php do_shortcode('[CAF_RESETPASSWORD]');?>
+	</div>
+</section>
+<?php
+  get_footer();
---
+++ accounts/register.php
@@ -0,0 +1,13 @@
+<?php
+  get_header();
+?>
+<section>
+	<header class="text-center margin-bottom-30">
+		<h2 class="section-title">Register</h2>
+	</header>
+	<div class="container">
+	    <?php do_shortcode('[CAF_REGISTER]');?>
+	</div>
+</section>
+<?php
+  get_footer();
