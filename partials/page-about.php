867ad2a8450bcd378c2b584b9f5699724f2cf5c3
Ralph John Galindo <ralphjesy@gmail.com>
2016-09-05 13:38:08 +0800

    Random Quotes on About Page

--- partials/page-about.php
+++ partials/page-about.php
@@ -6,35 +6,36 @@
 			<header class="text-center margin-bottom-50 tiny-line">
 				<h2 class="font-proxima uppercase">WHO WE <span>ARE</span></h2>
 			</header>
-			
-		<blockquote class="quote">
-			<p> We’re not here to dazzle with science, we’re here to help; we’re different from the rest and we make no apologies for it.</p>
-			<cite>Web Anonymouse</cite>
-		</blockquote>
+
+			<blockquote class="quote">
+				<?php echo do_shortcode('[quotcoll limit="1" orderby="random"]')?>
+			</blockquote>
 			<div class="row">
-			
+
 				<div class="col-lg-3 col-md-3 col-sm-3 text-center">
 					<img class="img-responsive" src="<?=get_stylesheet_directory_uri();?>/assets/img/mmc-logo.png" alt="">
 				</div>
 
 				<div class="col-md-9 col-sm-9">
 
-					<p>This is a website aimed at both the discerning expert investor who needs to keep abreast of current events, market movements and new investment opportunities, 
+					<p>This is a website aimed at both the discerning expert investor who needs to keep abreast of current events, market movements and new investment opportunities,
 and also the ambitious novice individual who needs some simple financial advice and a guiding light to point the way and simplify the process,
- thus allowing everyone to gain an advantage in the weird and wonderful world of investment and personal wealth accumulation. 
+ thus allowing everyone to gain an advantage in the weird and wonderful world of investment and personal wealth accumulation.
 </p>
 <p>We are a crowd-sourced content aggregator for the financial markets, provided by successful investors and industry experts. We offer up articles, experts&#8217; insight with investment advice, and investment strategies which will enable any motivated and aspiring individual to realize their dream of a long, wealthy and stress-free retirement.</p>
 
-					
+
 				</div>
 
 			</div>
-			
+
 			<hr/>
 			<div class="row">
-			
+
 				<div class="col-lg-3 col-md-3 col-sm-3 text-center">
-					<img class="img-responsive padding-20" src="<?=get_stylesheet_directory_uri();?>/assets/img/dv-logo-blk.png" alt="">
+					<a href="www.divestmedia.com">
+						<img class="img-responsive padding-20" src="<?=get_stylesheet_directory_uri();?>/assets/img/dv-logo-blk.png" alt="">
+					</a>
 				</div>
 
 				<div class="col-md-9 col-sm-9">
@@ -42,8 +43,7 @@ and also the ambitious novice individual who needs some simple financial advice 
 <p>What else does anybody really want from life? Health and wealth, in that order, with the latter acting as a guarantor for the former; we can’t get this by wishing it to come to us, we can only get it by going after it. After all, it’s true that you can’t get everything in life, but you can still give it a good go. This site’s purpose is to lend a helping hand to the valiant few in search of financial advice and investment opportunities who’ve made the right choice of joining the investment magic circle.</p>
 
 <blockquote class="pull-right">
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-	<cite>Albert Einstein</cite>
+	<?php echo do_shortcode('[quotcoll limit="1" orderby="random"]')?>
 </blockquote>
 <p>This is a website aimed at the successful businessman or smart entrepreneur who are looking to further increase their wealth and ensure that their riches have a chance to grow exponentially, thus guaranteeing that bright and stable future we all want. It’s aimed at the individual investor who is determined to increase their personal wealth and not let the grass grow under their feet. It’s aimed at those who are shrewd enough to recognize that riches don’t come to you, and who are brave enough to go after what they want. It’s aimed at novice investors who just need some information and basic investment advice. It’s aimed at all of us out there who get intimidated by complicated investor sites seemingly designed to be daunting. It’s aimed at ordinary successful business people who have decided to not be overawed or fazed any longer. It’s aimed at all of us.</p>
 <p>We also provide a forum whereby leading experts from different fields of investment can have a platform, by way of featured articles, from which to offer guidance and financial advice, and where beginner investors can find the right financial advisor to help them prosper. We offer unbiased investment advice and conflict-free counsel from experienced analysts with long-term successful track records, and we present it in a user-friendly and concise format so that it actually does what it’s supposed to do; help you. We will also strive to present you with new and exciting investment opportunities whenever we can. And we’re also going to review and assess other sites, forums and financial aids, and we’re going to tell the truth.</p>
@@ -52,10 +52,10 @@ and also the ambitious novice individual who needs some simple financial advice 
 		</div>
 	</section>
 	<!-- / -->
-	
+
 	<!-- ABOUT -->
 	<section id="about_heads" class="about_heads parallax dark alternate primary-bg" style="background: #1ab05d url('fxaces.jpg') repeat top left!important ;">
-		
+
 		<div class="overlay dark-9"><!-- dark overlay [1 to 9 opacity] --></div>
 		<div class="container">
 
@@ -63,10 +63,10 @@ and also the ambitious novice individual who needs some simple financial advice 
 				<h3 class="size-26 uppercase">Biographies of <span>Our Team</span></h3>
 				<p class="lead font-lato"></p>
 				<hr />
-				
+
 			</header>
 
-			
+
 			<div class="row margin-bottom-20">
 				<!-- <div class="col-sm-6 col-md-3 item">
 					<div class=" noradius">
@@ -112,7 +112,7 @@ and also the ambitious novice individual who needs some simple financial advice 
 							<div class="box2" style="background-color:#a0ce4e;">
 								<h4>Cameron Clark</h4>
 								<hr />
-								<p> 
+								<p>
 Cameron Clark
 Cameron has been involved in the investment world since the early 1980’s when he started out a career in investment consultancy, providing expert advice in corporate off shore investment. He moved over to Zurich Financial Services where he became a specialized broker consultant, dealing with IFA’s, accountants and solicitors and bringing in corporate business in both on and off shore, and also dealing with group pensions. He moved to Asia in 2003 where he moved to the sales side of the business, before then investing his money in various stocks and commodities and eventually breaking out and becoming an independent investor forging his own path. He has a wealth of experience in both investing and business management, as well as sales and marketing. He has been making his money grow through investments for the last two decades.</p>
 <button class="btn btn-xs btn-primary btn-team-show-more">show more</button>
@@ -168,12 +168,12 @@ He has lived in the UK, France, Germany and Spain, and has been living in Asia s
 		</div>
 	</section>
 	<!-- / -->
-	
+
 	<section class="about_heads">
 		<div class="container">
 			<div class="row margin-bottom-40">
 				<div class="col-sm-4 col-md-3">
-					
+
 				</div>
 				<div class="col-md-3 col-sm-4 item">
 					<div class="box-flip box-color box-icon box-icon-center box-icon-round box-icon-large text-center">
@@ -190,7 +190,7 @@ He has lived in the UK, France, Germany and Spain, and has been living in Asia s
 							<div class="box2" style="background-color:#a0ce4e;">
 								<h4>Anwar Ratag</h4>
 								<hr />
-								<p>Anwar has more than two decades of experience in Visual/Creative and Multimedia Design, as well as over ten years of involvement in Web Design and Development, and he has worked for various Network, Multimedia, Ad Agency and Online Gaming companies. 
+								<p>Anwar has more than two decades of experience in Visual/Creative and Multimedia Design, as well as over ten years of involvement in Web Design and Development, and he has worked for various Network, Multimedia, Ad Agency and Online Gaming companies.
 
 An artist since birth, with exceptional skills in traditional and digital arts, he loves drawing, sketching and painting, and is also highly skilled in photography and videography and 3-D animation, as well as being well versed in HTML and CSS, and with extensive knowledge in PHP and Java. Also an avid fan of heavy metal music and video games. He just loves anything computer-related.</p>
 <button class="btn btn-xs btn-primary btn-team-show-more">show more</button>
@@ -213,7 +213,7 @@ An artist since birth, with exceptional skills in traditional and digital arts, 
 							<div class="box2" style="background-color:#a0ce4e;">
 								<h4>Charlie Apostol</h4>
 								<hr />
-								<p>Charlie brings more than a decade of experience in overall business operations including training and evaluation, budget forecasting and control, and overseeing general business functions such as Customer Service, Payment, Fraud Control, and Risk Management. He has successfully set up online casinos, spearheaded the creation of company documents, and formulated business continuity plans. If it’s anything to do with running a business, he’s been there, done that, and overseen the design of the t-shirt. 
+								<p>Charlie brings more than a decade of experience in overall business operations including training and evaluation, budget forecasting and control, and overseeing general business functions such as Customer Service, Payment, Fraud Control, and Risk Management. He has successfully set up online casinos, spearheaded the creation of company documents, and formulated business continuity plans. If it’s anything to do with running a business, he’s been there, done that, and overseen the design of the t-shirt.
 
 Motivated through inspiring others to perform above and beyond expectations, he spreads enthusiasm and promotes inventive thinking while keeping it all structured and on track. Also ardent on both the music and culinary world, he ventured into music-inspired bistros. Good food, good music. Just a few things he loves about life.</p>
 <button class="btn btn-xs btn-primary btn-team-show-more">show more</button>
@@ -221,7 +221,7 @@ Motivated through inspiring others to perform above and beyond expectations, he 
 						</div>
 					</div>
 				</div>
-				
+
 			</div>
 		</div>
 	</section>
