56caaf2a2d0bdbedc2811d0c8f0ff5a143ea46e8
ljopleda <ljopleda@gmail.com>
2016-09-07 13:19:23 +0800

    Adding child to telling tales child

--- partials/page-tellingtales-child.php
+++ partials/page-tellingtales-child.php
@@ -76,7 +76,7 @@
 						<header class="text-center margin-top-40 margin-bottom-50 tiny-line">
 							<h2 class="font-proxima uppercase"><?=the_title()?></span></h2>
 						</header>
-					<?php 
+					<?php
 						if(!strcasecmp(basename(get_permalink()),'quotes-quotes-quotes')){
 							global $wpdb;
 							$_tablename = $wpdb->prefix.'quotescollection';
@@ -101,60 +101,44 @@
 							</div>
 						</article>
 					<?php endwhile;?>
-					<div class="investment-assets">
+					<div class="row">
 						<?php
-						$args = array(
-						    'post_type'      => 'post',
-						    'posts_per_page' => -1,
-						    'category_name'    => basename(get_permalink()),
-						    'order'          => 'ASC',
-						    'orderby'        => 'menu_order'
-						 );
-
-
-						$parent = new WP_Query( $args );
-
-						if ( $parent->have_posts() ) : ?>
-							<div class="row margin-bottom-30">
-						    <?php
-						    	$ctr = 0;
-						    	while ( $parent->have_posts() ) : $parent->the_post();
-
-						    	?>
-
-										<div class="col-md-6">
-											<div class="box-flip box-icon box-icon-round box-icon-large">
-												<div class="front">
-													<div class="box1" style="padding: 0;">
-														<figure style="background-image: url('<?=the_post_thumbnail_url()?>');background-size: cover;background-repeat: no-repeat;height: 250px;">
-														</figure>
-														<div class="section-content">
-															<div class="text-left">
-																<h4 class="title"><strong><?=the_title()?></strong></h4>
-																<label><?=xyr_smarty_limit_chars(strip_tags(html_entity_decode(get_the_content())),150);?></label>
-															</div>
-														</div>
+							$args = array(
+								'post_type'      => 'page',
+								'posts_per_page' => -1,
+								'post_parent'    => $post->ID,
+								'order'          => 'ASC',
+								'orderby'        => 'menu_order'
+							);
+							$parent = new WP_Query( $args );
+							if ( $parent->have_posts() ) : ?>
+							<?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
+								<div class="col-md-6 margin-bottom-30">
+									<div class="box-flip box-icon box-icon-center box-icon-round box-icon-large text-center">
+										<div class="front">
+											<div class="box1" style="padding: 0;">
+												<a href="<?=get_the_permalink()?>">
+													<figure style="border-bottom: 5px solid #1ecd6e;background-image: url('<?=the_post_thumbnail_url()?>');background-size: cover;background-repeat: no-repeat;height: 250px;"></figure>
+												</a>
+												<span class="section-content">
+													<div class="text-left">
+														<h4 class="title"><strong><?=the_title()?></strong></h4>
+														<label><?=xyr_smarty_limit_chars(strip_tags(html_entity_decode(get_the_content())), 150);?></label>
 													</div>
-												</div>
-												<div class="back">
-													<div class="box2">
-														<a href="<?=get_the_permalink()?>"><h4><?=the_title()?></h4></a>
-														<hr />
-														<p><?=xyr_smarty_limit_chars(strip_tags(html_entity_decode(get_the_content())), 500);?></p>
-														<a href="<?=get_the_permalink()?>" class="link-read-more">Read more</a>
-													</div>
-												</div>
+												</span>
 											</div>
 										</div>
-						    <?php
-						    if($ctr++==1){
-						    		echo '</div>
-							<div class="row margin-bottom-30">';
-									$ctr = 0;
-						    	}
-						    endwhile; ?>
-						    </div>
-						<?php endif; wp_reset_query(); ?>
+										<div class="back">
+											<div class="box2">
+												<a href="<?=get_the_permalink()?>"><h4><?=the_title()?></h4></a>
+												<hr />
+												<p><?=xyr_smarty_limit_chars(strip_tags(html_entity_decode(get_the_content())), 500)?></p>
+											</div>
+										</div>
+									</div>
+								</div>
+							<?php endwhile; ?>
+							<?php endif; wp_reset_query(); ?>
 					</div>
 				</div>
 				<?php
