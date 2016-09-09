759050a9c3eb69481d85cc4afffac7373e7dd811
Ralph John Galindo <ralphjesy@gmail.com>
2016-09-09 11:37:23 +0800

    Adding Links to Homepage Ads

--- partials/content-ads-2.php
+++ partials/content-ads-2.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <a href="#">
+                <a href="<?=site_url('stockwatch')?>">
                     <img src="<?=site_url('/wp-content/uploads/sites/8/2016/09/BANNERNEW-1.gif')?>" class="img-responsive"/>
                 </a>
             </div>
