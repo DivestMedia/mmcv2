d25b89f3899fc4a7de860bf57d01b01c89aa5b15
Ralph John Galindo <ralphjesy@gmail.com>
2016-09-09 10:59:14 +0800

    Adding Shortcode on contact us page

--- partials/content-contactus.php
+++ partials/content-contactus.php
@@ -1,36 +1,37 @@
-	<section class="alternate dark liner">
-		<div class="container">
-			<header class="text-center margin-bottom-50 tiny-line">
-				<span class="uppercase letter-spacing-10 size-10">Have Questions?</span>
-				<h2 class="font-proxima uppercase">Get in<span> Touch</span></h2>
-			</header>
+<section class="alternate dark liner">
+	<div class="container">
+		<header class="text-center margin-bottom-50 tiny-line">
+			<span class="uppercase letter-spacing-10 size-10">Have Questions?</span>
+			<h2 class="font-proxima uppercase">Get in<span> Touch</span></h2>
+		</header>
 
 
-			<div class="row box-gradient box-black">
-				<div class="col-xs-6 col-sm-3 cols-primary">
-					<i class="fa fa-comments-o fa-4x"></i>
-					<br/><br/>
-					<small class="text-gray">LIVE CHAT</small>
-					<p>Customer Support</p>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<i class="fa fa-mobile fa-4x"></i>
-					<br/><br/>
-					<small class="text-gray">PHONE NUMBER</small>
-					<p>(632) 555-1734</p>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<i class="fa fa-envelope-o fa-4x"></i>
-					<br/><br/>
-					<small class="text-gray">EMAIL</small>
-					<p>support@marketmasterclass.com</p>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<i class="fa fa-map-marker fa-4x"></i>
-					<br/><br/>
-					<small class="text-gray">ADDRESS</small>
-					<p>#2698 CBL Building Gen. Cailles St, Brgy. Bangkal, Makati City , PH 1233 </p>
-				</div>
+		<div class="row box-gradient box-black">
+			<div class="col-xs-6 col-sm-3 cols-primary">
+				<i class="fa fa-comments-o fa-4x"></i>
+				<br/><br/>
+				<small class="text-gray">LIVE CHAT</small>
+				<p style="height:24px;">Customer Support</p>
+				<?php do_shortcode("[dmchatstatus]"); ?>
+			</div>
+			<div class="col-xs-6 col-sm-3">
+				<i class="fa fa-mobile fa-4x"></i>
+				<br/><br/>
+				<small class="text-gray">PHONE NUMBER</small>
+				<p>(632) 555-1734</p>
+			</div>
+			<div class="col-xs-6 col-sm-3">
+				<i class="fa fa-envelope-o fa-4x"></i>
+				<br/><br/>
+				<small class="text-gray">EMAIL</small>
+				<p>support@marketmasterclass.com</p>
+			</div>
+			<div class="col-xs-6 col-sm-3">
+				<i class="fa fa-map-marker fa-4x"></i>
+				<br/><br/>
+				<small class="text-gray">ADDRESS</small>
+				<p>#2698 CBL Building Gen. Cailles St, Brgy. Bangkal, Makati City , PH 1233 </p>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
