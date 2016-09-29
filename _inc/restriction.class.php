4fc8980f12fb9e6b5b70989fe96b34b54e9a9ef7
Ralph John Galindo <ralphjesy@gmail.com>
2016-09-29 10:18:31 +0800

    Page Restriction Initial Implementation

--- _inc/restriction.class.php
+++ _inc/restriction.class.php
@@ -3,28 +3,67 @@
 
 
 class memberRestriction {
-	
-	
-	
+
+	public $restricted_pages = [
+		'dummies-guide',
+		'telling-tales',
+		'find-a-broker',
+	];
+
 	public function __construct(){
-		
-		
+		add_action('init',[&$this,'hooks']);
 	}
-	
+
+	public function restrict_content($content){
+		global $post;
+
+		if($this->ispostrestricted($post)){
+			$content = 'Content Restricted';
+		}
+		return '';
+	}
+
+	public function ispostrestricted($post){
+		// Restriction Rules
+		$restrict = 0;
+		// Getting Current posts parent
+		$ancestors = get_post_ancestors($post->ID);
+		$parent = $post->ID;
+
+		if(count($ancestors)){
+			$parent = $ancestors[count($ancestors) - 1];
+		}
+
+		// If page child of dummies-guide
+		if(
+		in_array(get_post($parent)->post_name,$this->restricted_pages)
+		){
+			$restrict++;
+		}
+
+		return $restrict!=0;
+	}
+
+	public function restrict_page(){
+
+		if(is_user_logged_in()) return false;
+
+		global $post;
+
+		return $this->ispostrestricted($post);
+	}
+
 	public function hooks(){
-		
+
+		add_filter('the_content', [&$this, 'restrict_content']);
+		add_shortcode( 'dm_restrict_page' , [&$this,'restrict_page'] );
+
 	}
-	
+
 	public function out(){
-		
-		
+
 	}
-	
-	
-	
-	
-	
+
 }
-
-
-memberRestriction();
+$restrict = new memberRestriction();
+$_GLOBALS['restrict'] = $restrict;
